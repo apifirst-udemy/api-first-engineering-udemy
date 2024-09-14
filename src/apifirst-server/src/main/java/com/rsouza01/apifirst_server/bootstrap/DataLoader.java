ab3247c48343631e24475751dfc7fec79769ce84
Rodrigo de Souza <rsouza01@gmail.com>
2024-09-14 11:02:56 +0000

    Mock data added

---
+++ src/apifirst-server/src/main/java/com/rsouza01/apifirst_server/bootstrap/DataLoader.java
@@ -0,0 +1,72 @@
+package com.rsouza01.apifirst_server.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.rsouza01.apifirst.model.Address;
+import com.rsouza01.apifirst.model.Customer;
+import com.rsouza01.apifirst.model.Name;
+import com.rsouza01.apifirst.model.PaymentMethod;
+import com.rsouza01.apifirst_server.repositories.CustomerRepository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Address address1 = Address.builder()
+                .addressLine1("1234 W Some Street")
+                .city("Some City")
+                .state("FL")
+                .zip("33701")
+                .build();
+
+        Customer customer1 = Customer.builder()
+                .name(Name.builder()
+                        .firstName("John")
+                        .lastName("Thompson")
+                        .build())
+                .billToAddress(address1)
+                .shipToAddress(address1)
+                .email("john@springframework.guru")
+                .phone("800-555-1212")
+                .paymentMethods(List.of(PaymentMethod.builder()
+                        .cardNumber(12341234)
+                        .expiryMonth(12)
+                        .expiryYear(26)
+                        .build()))
+                .build();
+
+        Address address2 = Address.builder()
+                .addressLine1("1234 W Some Street")
+                .city("Some City")
+                .state("FL")
+                .zip("33701")
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .name(Name.builder()
+                        .firstName("Jim")
+                        .lastName("Smith")
+                        .build())
+                .billToAddress(address2)
+                .shipToAddress(address2)
+                .email("jim@springframework.guru")
+                .phone("800-555-1212")
+                .paymentMethods(List.of(PaymentMethod.builder()
+                        .cardNumber(1234888)
+                        .expiryMonth(12)
+                        .expiryYear(26)
+                        .build()))
+                .build();
+
+        customerRepository.save(customer1);
+        customerRepository.save(customer2);
+    }
+}
--- src/apifirst-server/src/test/java/com/rsouza01/apifirst_server/ApifirstServerApplicationTests.java
+++ src/apifirst-server/src/test/java/com/rsouza01/apifirst_server/ApifirstServerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.rsouza01.apifirst_server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rsouza01.apifirst_server.repositories.CustomerRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApifirstServerApplicationTests {
+
+	@Autowired
+	CustomerRepository customerRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testDataLoad() {
+		assertThat(customerRepository.count()).isGreaterThan(0L);
+	}
 }
