e92aa6f19dfa8e9769fbead514618a3946d2680f
Rodrigo de Souza <rsouza01@gmail.com>
2024-11-11 16:37:50 +0000

    Test errors fixed

--- src/apifirst-server/src/test/java/com/rsouza01/apifirst_server/services/CustomerServiceImplTest.java
+++ src/apifirst-server/src/test/java/com/rsouza01/apifirst_server/services/CustomerServiceImplTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@Disabled
 class CustomerServiceImplTest {
 
     @Autowired
